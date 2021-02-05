898632eec34a99a2a67df935a9ece6655e55af26
aszpatowski <314211@uwr.edu.pl>
2021-02-05 01:35:51 +0100

    Zadanie

--- script.py
+++ script.py
@@ -2,18 +2,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import solve_ivp
 
-f = lambda t, Y: (Y[1], - 24 * Y[0] - 11 * Y[1])
+f = lambda t, Y: (Y[1], - 24 * Y[0] - 11 * Y[1])  # Przekształcona funkcja
 
-y_analitic = lambda t: 1.4 * np.exp(-8 * t) - 1.4 * np.exp(-3 * t)
+ekstremum = lambda t, Y: Y[1]  # Znajdowanie miejsca zerowego y'(t)
 
-t_extreme_analitic = -np.log(3 / 8) / 5
+y_analitic = lambda t: 1.4 * np.exp(-8 * t) - 1.4 * np.exp(-3 * t)  # Obliczenia na githubie
 
-y0 = [0, -7]
+t_extreme_analitic = -np.log(3 / 8) / 5  # Obliczenia na githubie
 
+y0 = [0, -7]  # y(0) = 0, y'(0) = -7
+# t = [0,1]
 tp = 0
 tk = 1
-t = np.linspace(tp, tk, 51)  # 21 do wyników, 51 do wykresu, 100001 do liczenia ekstremum
-y = solve_ivp(f, [tp, tk], y0, t_eval=t)  # Domyślna metoda to RK45
+t = np.linspace(tp, tk, 51)  # 21 do wyników, 51 do wykresu
+y = solve_ivp(f, [tp, tk], y0, t_eval=t, dense_output=True, events=ekstremum)  # Domyślna metoda to RK45
 y_analitic_points = [y_analitic(t_point) for t_point in t]
 
 print(f"{'t':10s}{'y(t)':22s}")
@@ -26,10 +28,8 @@ plt.plot(t, y_analitic_points)
 plt.plot(y.t, y.y[0], '.')
 
 # liczenie ekstremum
-t_ekstr = y.t[np.abs(y.y[1]).argmin()]
-
-y_ekstr = y.y[0][np.argwhere(y.t == t_ekstr)[0][0]]
-
+t_ekstr = y.t_events[0][0]
+y_ekstr = y.y_events[0][0][0]
 print("Położenie ekstremum")
 print(f"y({t_ekstr}) = {y_ekstr}")
 print("Ekstremum policzone analitycznie")
